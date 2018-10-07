70e34b8e2551d740f13f4408f07232d129440803
Mwiza MBP 2017 <mwiza@agripredict.com>
2018-10-08 00:44:31 +0200

    Renamed asset files

---
+++ cards.py
@@ -0,0 +1,16 @@
+from aluLib import *
+
+# Screen width and height
+window_width = 1000
+window_height = 900
+
+img = load_image("paddle.png")
+## somewhere in a drawing function called by start_graphics:
+draw_image(img, 100, 150)
+
+def cards():
+    card_list = ['']
+def main():
+    pass
+
+start_graphics(main, width=window_width, height=window_height, framerate=60)
