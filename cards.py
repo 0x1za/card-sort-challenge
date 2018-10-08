cbeada0ab09a7af45e9aadd007d8d30d7263daaa
Mwiza MBP 2017 <mwiza@agripredict.com>
2018-10-08 10:44:56 +0200

    Renamed asset files

--- cards.py
+++ cards.py
@@ -1,24 +1,59 @@
 from aluLib import *
-import os
 
 # Screen width and height
 window_width = 1500
-window_height = 900
-
+window_height = 400
 
 def cards():
+    global loaded, inserted
     # Load cards from assets/ directory.
-    card_list = os.listdir('assets/')
-    x = 20
-    for i in card_list:
-        draw_image("assets/"+ i, x, 150)
-        x+=110
+    if loaded == False:
+        inserted = raw_input("Please select cards from A,2,3,4,5,6,7,8,9,10,J,K or Q: ")
+        inserted = inserted.split(',')
 
-def sort(cards):
+        x = 20
+        draw_text("You have selected the cards below, Press s to sort and r to randomize selection", window_width/3, 40)
+        for i in inserted:
+            draw_image("assets/"+ i+'.png', x, 150)
+            x+=110
+        loaded = True
+
+def sort():
+    global sorted, status, final
     if is_key_pressed('s'):
-        pass
+        sorted = []
+        deck = ['A', 2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'K', 'Q']
+        for x in deck:
+            for i in inserted:
+                if str(x) == i:
+                    sorted.append(i)
+                else:
+                    pass
+        status = True
+        final = sorted
+
+
+def draw():
+    global status, loaded_sort
+    if status == True:
+        clear()
+        draw_text("Your sorted card deck is below", window_width / 4,
+                  40)
+        x = 20
+        for i in sorted:
+            draw_image("assets/" + i + '.png', x, 150)
+            x += 110
+        loaded_sort = True
+
 
 def main():
     cards()
+    sort()
+    draw()
 
-start_graphics(main, width=window_width, height=window_height, framerate=60)
+loaded = False
+loaded_sort = False
+inserted = []
+final = []
+status = None
+start_graphics(main, width=window_width, height=window_height)
