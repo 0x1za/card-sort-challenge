b96170904f39c82b9d19d2b2f203e762a2deccaf
Mwiza MBP 2017 <mwiza@agripredict.com>
2018-10-24 16:05:25 +0200

    Bug has been fixed.

--- chrome.py
+++ chrome.py
@@ -2,24 +2,19 @@ x = [1, 2, 3, 4]
 
 
 
-# def reverse(x):
-#     length = len(x) - 1
-#
-#     y = length / 2
-#
-#     i = 0
-#     while i <= y:
-#         new = x[length - i]
-#         old = x[i]
-#         x[i], x[length - i] = new, old
-#
-#         i += 1
-#
-#     return x
-#
-# print (reverse(x))
+def reverse(x):
+    length = len(x) - 1
 
-def simp_rev(x):
-    print (x[::-1])
+    y = length / 2
 
-simp_rev(x)
+    i = 0
+    while i <= y:
+        new = x[length - i]
+        old = x[i]
+        x[i], x[length - i] = new, old
+
+        i += 1
+
+    return x
+
+print (reverse(x))
