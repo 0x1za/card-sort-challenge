d83c55281f74e82e23053c2366f4cd5c9a094eaa
Mwiza MBP 2017 <mwiza@agripredict.com>
2018-10-24 16:02:11 +0200

    Reversed order

--- chrome.py
+++ chrome.py
@@ -2,19 +2,24 @@ x = [1, 2, 3, 4]
 
 
 
-def reverse(x):
-    length = len(x) - 1
+# def reverse(x):
+#     length = len(x) - 1
+#
+#     y = length / 2
+#
+#     i = 0
+#     while i <= y:
+#         new = x[length - i]
+#         old = x[i]
+#         x[i], x[length - i] = new, old
+#
+#         i += 1
+#
+#     return x
+#
+# print (reverse(x))
 
-    y = length / 2
+def simp_rev(x):
+    print (x[::-1])
 
-    i = 0
-    while i <= y:
-        new = x[length - i]
-        old = x[i]
-        x[i], x[length - i] = new, old
-
-        i += 1
-
-    return x
-
-print (reverse(x))
+simp_rev(x)
